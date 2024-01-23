5d56582024f8b261b286cfb3c1fc6f5fe0024a63
Gaganjot <gaganjothans777@gmail.com>
2024-01-23 13:16:11 -0500

    Header, Home and Product components created

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/71DM0VoXW2L._SX3000_.jpg"
+          alt=""
+        />
+
+        <div className="home__row">
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row"></div>
+        <div className="home__row"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Product.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Product.css";
+import StarIcon from "@mui/icons-material/Star";
+
+function Product() {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean startup</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div className="product__rating">
+          <p>
+            <StarIcon />
+          </p>
+        </div>
+      </div>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        alt=""
+      />
+      <button>Add to basket</button>
+    </div>
+  );
+}
+
+export default Product;
