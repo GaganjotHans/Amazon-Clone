c5e766f47d8eebb6a10a0532aa5eb00c26aeae56
Gaganjot <gaganjothans777@gmail.com>
2024-01-27 12:48:50 -0500

    Login Page added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <Router>
+      <div className="App">
         <Routes>
           <Route
             path="/"
@@ -28,9 +28,10 @@ function App() {
               </>
             }
           />
+          <Route path="/login" element={<Login />} />
         </Routes>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    // Don't like refreshing page in react
+    e.preventDefault();
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://amazon-blogs-brightspot-lower.s3.amazonaws.com/about/00/92/0260aab44ee8a2faeafde18ee1da/amazon-logo-inverse.svg"
+          alt=""
+        />
+      </Link>
+
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form action="">
+          <h5>Email</h5>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
+        </form>
+        <p>
+          By continuing, you agree to Amazon's Fake Clone Conditions of Use and
+          Privacy Notice.
+        </p>
+        <button className="login__registerButton" onClick={register}>
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
